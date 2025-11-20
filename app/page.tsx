6c14ddc84995c4b722fa6c53007cf3d82b1a3f2b
danishayman <danishaiman3b@gmail.com>
2025-11-21 00:40:21 +0800

    rename section files

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import HeroSection from "@/components/sections/HeroSection";
-import WhyIBuiltThisSection from "@/components/sections/WhyIBuiltThisSection";
-import MaterialsSection from "@/components/sections/MaterialsSection";
-import ToolsSection from "@/components/sections/ToolsSection";
-import PlansSection from "@/components/sections/PlansSection";
-import BuildProcessSection from "@/components/sections/BuildProcessSection";
-import ElectronicsSection from "@/components/sections/ElectronicsSection";
-import FirstFlightSection from "@/components/sections/FirstFlightSection";
-import TroubleshootingSection from "@/components/sections/TroubleshootingSection";
-import ModificationsSection from "@/components/sections/ModificationsSection";
-import GallerySection from "@/components/sections/GallerySection";
-import WhatILearnedSection from "@/components/sections/WhatILearnedSection";
-import ResourcesSection from "@/components/sections/ResourcesSection";
-import FAQSection from "@/components/sections/FAQSection";
-import ContactSection from "@/components/sections/ContactSection";
+import HeroSection from "@/components/sections/Hero";
+import WhyIBuiltThisSection from "@/components/sections/WhyIBuiltThis";
+import MaterialsSection from "@/components/sections/Materials";
+import ToolsSection from "@/components/sections/Tools";
+import PlansSection from "@/components/sections/Plans";
+import BuildProcessSection from "@/components/sections/BuildProcess";
+import ElectronicsSection from "@/components/sections/Electronics";
+import FirstFlightSection from "@/components/sections/FirstFlight";
+import TroubleshootingSection from "@/components/sections/Troubleshooting";
+import ModificationsSection from "@/components/sections/Modifications";
+import GallerySection from "@/components/sections/Gallery";
+import WhatILearnedSection from "@/components/sections/WhatILearned";
+import ResourcesSection from "@/components/sections/Resources";
+import FAQSection from "@/components/sections/FAQ";
+import ContactSection from "@/components/sections/Contact";
 
 export default function Home() {
   return (
