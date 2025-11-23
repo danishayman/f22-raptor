9781aa5bd8d6582bf67a8914f3f9805a6df1848a
danishayman <danishaiman3b@gmail.com>
2025-11-24 00:43:59 +0800

    feat: restructure CADImagesSection to display multiple CAD model views

--- components/sections/CADImages.tsx
+++ components/sections/CADImages.tsx
@@ -8,7 +8,14 @@ export default function CADImagesSection() {
       <div className="container mx-auto max-w-6xl">
         <SectionHeader title="CAD Images" subtitle="3D models and renderings" />
         <div className="space-y-6">
-          <ImageBlock label="CAD Model" aspectRatio="video" figNumber="03" />
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <ImageBlock src="/CAD-images/f22_4.png" label="3D Model View 1" aspectRatio="custom" figNumber="03" />
+            <ImageBlock src="/CAD-images/f22_3.png" label="3D Model View 2" aspectRatio="custom" figNumber="04" />
+            <ImageBlock src="/CAD-images/f22_front.png" label="Front View" aspectRatio="custom" figNumber="05" />
+            <ImageBlock src="/CAD-images/f22_side.png" label="Side View" aspectRatio="custom" figNumber="06" />
+            <ImageBlock src="/CAD-images/f22_top.png" label="Top View" aspectRatio="custom" figNumber="07" />
+            <ImageBlock src="/CAD-images/f22_layout.png" label="Layout View" aspectRatio="custom" figNumber="08" />
+          </div>
           <TextBlock>
             <p>Computer-aided design models and technical renderings of the F-22 Raptor build.</p>
           </TextBlock>
